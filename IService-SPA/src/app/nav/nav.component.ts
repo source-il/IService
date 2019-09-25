7403c101e00ae7a21b96282b1084ad21ae431f51
Simon <dotnet.il@gmail.com>
2019-09-25 04:35:06 +0300

    End Of section 5

--- IService-SPA/src/app/nav/nav.component.ts
+++ IService-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.autoService.login(this.model).subscribe(next => {
       console.log('Logged in successfuly');
     }, error => {
-      console.log('Failed to login');
+      console.log(error);
     });
   }
 
