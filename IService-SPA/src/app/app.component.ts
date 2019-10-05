41203fd594bc33da0f694a4df29785e1e9b12028
Simon <dotnet.il@gmail.com>
2019-10-05 23:40:30 +0300

    End Of Section 6

--- IService-SPA/src/app/app.component.ts
+++ IService-SPA/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'IService-SPA';
+
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.authService.jwtHelper.decodeToken(token);
+    }
+  }
+
 }
+
+
+
